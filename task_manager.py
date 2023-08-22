cc42341e59cfdead67b6fd177fe58960720a11d5
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2023-08-22 09:40:37 +0200

    Clean up code and comments

--- task_manager.py
+++ task_manager.py
@@ -3,6 +3,9 @@
 from datetime import datetime
 import calendar
 
+#====functions section==============
+
+# Converts the user.txt into a dictionary with usernames as keys and passwords as values
 def user_to_dic(file):
     dic = {}
     with open(file, "r") as user_file:
@@ -14,10 +17,15 @@ def user_to_dic(file):
             dic[user_and_password[0]] = user_and_password[1]
     return dic
 
+# Converts any .txt file into a list of lines
 def split_lines(file):
     file_data = file.read()
     line_list = file_data.splitlines()
     return line_list
+
+# Defining characters that will be required for the desired output format
+line = "\u2500" * 70
+output_space = "\t" * 3
 
 #====Login Section====
 '''Here you will write code that will allow a user to login.
@@ -27,11 +35,12 @@ def split_lines(file):
 '''
 while True:
     user_dic = user_to_dic("user.txt")
+
     username = input("Enter username: ")
     password = input("Enter password: ")
 
     # This reads the user file, checks if the user exists,
-    # checks if the password matches the user and asks the user to try again if not
+    # checks if the password matches the user and asks the user to try again if it does not
     if username not in user_dic:
         print("Username does not exist. Please try again")
         continue
@@ -64,12 +73,16 @@ s - statistics
             - Check if the new password and confirmed password are the same
             - If they are the same, add them to the user.txt file,
               otherwise present a relevant message'''
+        
+        # Restricts access to only 'admin'
         if username != "admin":
             print("Only username: 'admin' is allowed to register new users. Please exit and login as 'admin'")
             continue
 
         new_username = input("Enter new username: ")
 
+        # Creates a new password for the new user
+        # Has a password confirmation feature
         while True:
             new_password = input("New password: ")
             confirm_password = input("Confirm password: ")
@@ -79,7 +92,8 @@ s - statistics
             else:
                 print("Passwords do not match. Please try gain")
                 continue
-
+        
+        # Writes the new user to the user.txt file
         with open("user.txt", "a") as user_file:
             user_file.write(f"\n{new_username}, {new_password}")
 
@@ -99,19 +113,25 @@ s - statistics
             user_dic = user_to_dic("user.txt")
             user_for_task = input("Enter username: ")
 
+            # Checks if the user exists by looking for it in the dictionary
             if user_for_task not in user_dic:
                 print("Username does not exist. Please try again")
                 continue
             else: 
                 break
+
+        # Asks the user for the required details of the task    
         task_title = input("Enter task title: ")
         task_desrip = input("Enter task description: ")
         task_due_date = input("Enter the due date for the task: ")
         get_date = datetime.now()
         task_status = "No"
         today = (f"{get_date.day}  {calendar.month_name[get_date.month][:3]}  {get_date.year}")
+
+        # Gathers all the user input into a list that will eventually be joined
         task_final = [user_for_task, task_title, task_desrip, today, task_due_date, task_status]
 
+        # Writes the task to the task.txt file by joining the above list into a string
         with open("tasks.txt", "a") as task_file:
             task_file.write(f"\n{', '.join(task_final)}")
 
@@ -128,9 +148,7 @@ s - statistics
 
             each_task = split_lines(task_file)
 
-            line = "\u2500" * 70
-            output_space = "\t" * 3
-
+            # Loops through each task in tasks.txt and prints the details in the desired format
             for each_line in each_task:
                 task_list = each_line.split(", ")
                 print(f'''{line}\nTask:\t{output_space}{task_list[1]}\nAssigned to:{output_space}{task_list[0]}\nDate assigned:{output_space}{task_list[3]}\nDue date:{output_space}{task_list[4]}\nTask complete?{output_space}{task_list[5]}\nTask description:\n {task_list[2]}\n{line}\n''')
@@ -150,16 +168,17 @@ s - statistics
         with open("tasks.txt", "r") as task_file:
 
             each_task = split_lines(task_file)
-            line = "\u2500" * 70
-            output_space = "\t" * 3
             
+            # Loops through the current users tasks and prints the details in the desired format
             for each_line in each_task:
                 task_list = each_line.split(", ")
                 if task_list[0] == username:
                     print(f'''{line}\nTask:\t{output_space}{task_list[1]}\nAssigned to:{output_space}{task_list[0]}\nDate assigned:{output_space}{task_list[3]}\nDue date:{output_space}{task_list[4]}\nTask complete?{output_space}{task_list[5]}\nTask description:\n {task_list[2]}\n{line}\n''')
 
+    # This code displays company dtatistics to the admin only
     elif menu == 's':
-
+        
+        # Restricts access to admin only
         if username != "admin":
             print("Only username: 'admin' is allowed to view statistics. Please exit and login as 'admin'")
             continue
@@ -170,6 +189,7 @@ s - statistics
 
             task_count = 0
 
+            # Counts the number of tasks as it loops through each task
             for each_line in each_task:
                 task_count += 1
 
@@ -179,6 +199,7 @@ s - statistics
 
             user_count = 0
 
+            # Counts the number of users as it loops through each user
             for each_line in each_user:
                 user_count += 1
         
